2f202c025dc68f10b8db9706018b8c0303649ec6
cd-MiracleSeeker <114259912+cd-MiracleSeeker@users.noreply.github.com>
2025-02-27 10:58:28 +0100

    Added the first two tree annotation R scripts

---
+++ 4-1_functions.R
@@ -0,0 +1,264 @@
+
+# function for importing the specified tree and rerooting it
+import_tree <- function(path, outgroup = "none")
+{
+  # import treefile
+  tree <- read.tree(path)
+  # if no outgroup is specified, return unrooted tree
+  if (outgroup == "none")
+    {return(tree)}
+  else
+    {
+    # get the node for the outgroup for rerooting.
+    reroot_node <- ggtree::MRCA(tree, .node1 = outgroup)
+    # reroot at specified node in the tree
+    rerooted_tree <- ape::root(tree, reroot_node)
+    return(rerooted_tree)
+    }
+}
+
+# function for making a reference plot and saving it as a file
+make_reference <- function(phylotree, savepath)
+{
+  renamed_reftree <- rename_taxa(phylotree, data_matrix, key = 1, value = mainTipName)
+  pdf(file = paste(savepath), width = 30, height = 75)
+  reference_plot = ggtree(renamed_reftree, layout = "rectangular")
+  reference_plot = reference_plot + theme_tree2() + geom_tiplab(offset=0.03) + geom_text(aes(label=node),hjust=-.3) + geom_rootpoint()
+  print(reference_plot)
+  dev.off()
+}
+
+# A helper function that gets all descendant tips of a given node.
+# This version uses extract.clade to get tip labels.
+get_tips <- function(tree, node) {
+  clade <- extract.clade(tree, node)
+  return(clade$tip.label)
+}
+
+# Function to get the highest (maximal) pure clades for a given taxon group.
+get_pure_block_nodes <- function(taxgroup, tree, taxgroup_type) {
+  # Get the taxa (e.g., species IDs) that should be in this taxonomic group.
+  taxon_tips <- intersect(data_matrix$IDX[data_matrix[[taxgroup_type]] == taxgroup], tree$tip.label)
+  n_tips <- length(tree$tip.label)
+  pure_blocks <- c()
+  
+  # Loop over all internal nodes
+  for (node in (n_tips + 1):(n_tips + tree$Nnode)) {
+    node_tips <- get_tips(tree, node)
+    # Check if the node is pure (all its descendant tips are from taxon_tips)
+    if (all(node_tips %in% taxon_tips)) {
+      # Identify the parent node from the tree edge matrix.
+      parent <- tree$edge[tree$edge[, 2] == node, 1]
+      parent_is_pure <- FALSE
+      if (length(parent) > 0) {
+        parent_tips <- get_tips(tree, parent)
+        if (all(parent_tips %in% taxon_tips)) {
+          parent_is_pure <- TRUE
+        }
+      }
+      # Only record nodes that are not nested within a larger pure block.
+      if (!parent_is_pure) {
+        pure_blocks <- c(pure_blocks, node)
+      }
+    }
+  }
+  
+  # Return the set of maximal nodes that define pure blocks.
+  return(pure_blocks)
+}
+
+# Revised main function that uses the pure block approach for paraphyletic groups.
+get_mrca <- function(taxgroup, tree, taxgroup_type) {
+  # Get the list of taxa IDs corresponding to the taxon group.
+  taxon_tips <- intersect(data_matrix$IDX[data_matrix[[taxgroup_type]] == taxgroup], tree$tip.label)
+  
+  if (length(taxon_tips) > 1) {
+    # If the whole group is monophyletic, we get one block.
+    if (is.monophyletic(tree, taxon_tips)) {
+      return(getMRCA(tree, tip = taxon_tips))
+    } else {
+      # For paraphyletic groups, return the maximal pure blocks.
+      return(get_pure_block_nodes(taxgroup, tree, taxgroup_type))
+    }
+  } else if (length(taxon_tips) == 1) {
+    # For a single tip, simply return that tip's node.
+    return(MRCA(tree, .node1 = taxon_tips))
+  } else {
+    return("not in tree")
+  }
+}
+
+# function for annotating the plot with colours
+apply_annotation <- function(anno_plot, lb, cl, fill = "none", cl_type="taxGroup")
+{
+  # search for most recent common ancestors of given taxon group
+  node_vector <- get_mrca(lb, rerooted_tree, cl_type)
+  # if the taxon group is not in the tree, return unmodified plot
+  if (node_vector[1] == "not in tree" || length(node_vector) == 0)
+  {return(anno_plot)}
+  
+  # apply annotations for all clades in the tree
+  for (nd in node_vector)
+  {
+    # set the label for the given clade
+    anno_plot <- anno_plot + geom_cladelabel(node = nd, label = lb, color = cl, offset = .15,align = TRUE)
+    
+    # highlight nodes with specified parameters
+    if (fill == "full")
+    {anno_plot <- anno_plot + geom_hilight(node = nd,fill = cl,color = "black")}
+    else if (fill == "rim")
+    {anno_plot <- anno_plot + geom_hilight(node = nd, fill = "transparent", color = cl, linewidth = 0.75)}
+    else if (fill == "none")
+    {}
+  }
+  
+  # return the modified plot
+  return(anno_plot)
+}
+
+# function for annotating a ggtree phylo plot with colors and taxon group names
+annotate_by_taxgroup <- function(taxgroup, phyloplot, path="", fill="full", colorscheme="monochrome", col_vec=c(), include=c(), export=TRUE)
+{
+  tax_anno_plot <- phyloplot
+  color_count <- length(unique(data_matrix[[taxgroup]]))
+  # if the amount of colors given is equal to the amount of taxa
+  if (length(col_vec)!=0 && length(col_vec)==color_count)
+  { anno_colors <- col_vec }
+  # if there are more taxa than colors given
+  else if (length(col_vec)!=0 && length(col_vec)<color_count)
+  {  
+    col_diff <- color_count-length(col_vec)
+    col_rand <- randomColor(count=col_diff,hue=colorscheme, luminosity = "dark")
+    anno_colors <- c(col_vec,col_rand)
+  }
+  # if there are less taxa than colors given
+  else if (length(col_vec)!=0 && length(col_vec)>color_count)
+  { anno_colors <- col_vec[1:color_count] }
+  # if no colors were given
+  else
+  { anno_colors <- randomColor(count=color_count, hue=colorscheme, luminosity = "dark") }
+  
+  print(paste("=== Generating tree for ",taxgroup," ===",sep = ""))
+  index <- 1
+  for (entry in unique(data_matrix[[taxgroup]])){
+    print(paste(entry,anno_colors[index],sep=": "))
+    
+    if (length(include)==0)
+    { tax_anno_plot <- apply_annotation(tax_anno_plot,entry,anno_colors[index],fill=fill,cl_type=taxgroup) }
+    else if (length(include)>0 && entry %in% include)
+    { tax_anno_plot <- apply_annotation(tax_anno_plot,entry,anno_colors[index],fill=fill,cl_type=taxgroup) }
+    else
+    { tax_anno_plot <- apply_annotation(tax_anno_plot,entry,anno_colors[index],fill="none",cl_type=taxgroup) }
+    
+    index <- index+1
+  }
+  
+  if (export==TRUE)
+  {
+    if (length(include)==0)
+    { save_path <- paste(path,taxgroup,".pdf",sep='') }
+    else
+    { save_path <- paste(path,taxgroup,"_only-",paste(include,collapse="-"),".pdf",sep='')  }
+    # save the created plot as a pdf
+    pdf(file=save_path,width=35,height=75)
+    print(tax_anno_plot)
+    dev.off()
+    print(paste("Colored tree saved as",save_path))
+  }
+  else
+  { return(tax_anno_plot) }
+  
+}
+
+make_subtree <- function(input_node,clade,path,wd=10,ht=10)
+{
+  print(paste("=== Generating Subtree for ",clade," ===",sep=""))
+  # create pdf device for saving
+  save_path <- paste(path,clade,".pdf", sep="")
+  pdf(file=save_path, width=wd, height=ht)
+  # get all descendants of input node
+  tips <- c(getDescendants(renamed_tree,input_node))
+  # get subtree of input node and title it
+  subtree <- zoom(renamed_tree,tips,subtree = FALSE,main=paste("Subtree of ",clade,sep=""),align.tip.label=FALSE)
+  # print the tree to save it as pdf
+  print(subtree)
+  dev.off()
+  print(paste("Subtree tree saved as",save_path))
+}
+
+# Helper function: Compute ancestral states in a bottom-up (postorder) pass.
+fast_get_ancestral_state <- function(tree, tip_states) {
+  Ntip <- length(tree$tip.label)
+  Nnode <- tree$Nnode
+  total_nodes <- Ntip + Nnode
+  # Preallocate a vector for all node states; initializing with NA.
+  states <- rep(NA, total_nodes)
+  
+  # Fill in states for the tip nodes.
+  # Ensure tip_states is named with tip labels.
+  states[1:Ntip] <- tip_states[tree$tip.label]
+  
+  # Process internal nodes in numerical order (internal nodes are often > Ntip).
+  # Assuming that for a rooted tree, for every internal node the children have lower numbers.
+  for (node in (Ntip + 1):total_nodes) {
+    # Get children nodes for this internal node from the edge matrix.
+    children <- tree$edge[tree$edge[, 1] == node, 2]
+    child_states <- states[children]
+    # If any child has an NA state or the children disagree, mark as NA.
+    if (any(is.na(child_states)) || length(unique(child_states)) > 1) {
+      states[node] <- NA
+    } else {
+      states[node] <- unique(child_states)
+    }
+  }
+  return(states)
+}
+
+# Revised version of color_by_habitat using the fast ancestral state calculation.
+color_by_trait <- function(col_plot, char_col_name, cl) {
+  # Assume that rerooted_tree is your tree and tree_data is a tibble version obtained earlier.
+  Ntip <- length(rerooted_tree$tip.label)
+  
+  # Extract the trait states for the tips from tree_data.
+  # This assumes that tree_data is ordered such that the first Ntip rows correspond to tips.
+  tip_states <- tree_data[[char_col_name]][1:Ntip]
+  # Name the tip states with corresponding tip labels.
+  names(tip_states) <- rerooted_tree$tip.label
+  # Compute ancestral states for all nodes using the fast bottom-up approach.
+  ancestral_states <- fast_get_ancestral_state(rerooted_tree, tip_states)
+  # Add the ancestral state information to tree_data.
+  tree_data$char_in <- ancestral_states
+  
+  # Build the plot using ggtree, mapping the computed states onto branches and tips.
+  col_plot <- col_plot %<+% tree_data +
+    geom_tree(aes(shape = factor(char_in), color = factor(char_in))) +
+    geom_tippoint(aes(color = factor(char_in)), size = 3) +
+    scale_color_manual(
+      values = c("0" = "black", "1" = cl, "NA" = "gray"),
+      labels = c("not present", "present", "unknown")
+    ) +
+    labs(color = char_col_name, shape = char_col_name) +
+    theme(
+      legend.position = "top",
+      legend.title.position = "left",
+      legend.justification = c("left", "top"),
+      legend.title = element_text(size = 25),
+      legend.text = element_text(size = 20),
+      legend.background = element_rect(linewidth = 0.35, color = "grey"),
+      legend.margin = margin(6, 6, 6, 6),
+      legend.box = "vertical"
+    )
+  
+  return(col_plot)
+}
+
+# function for generating a tree that has the specified trait mapped onto it
+trait_tree <- function(save_path, treeplot, trait, color)
+{
+  print(paste("=== Generating Trait Tree for ",trait," colored in ",color," ===",sep=""))
+  pdf(file = save_path, width = 35, height = 75)
+  main_plot_trait <- color_by_trait(treeplot, trait, color)
+  print(main_plot_trait)
+  dev.off()
+  print(paste("Trait tree saved as",save_path))
+}
---
+++ 4_color_trees.R
@@ -0,0 +1,148 @@
+#!/usr/bin/env Rscript
+
+# remove all variables from environment
+rm(list=ls())
+# set working directory
+setwd("~/Documents/Programming/Bachelorarbeit/tree_recoloring/main")
+
+# load required libraries
+library(data.tree)
+library(ape)
+library(tidyverse)
+library(ggtree)
+library(treeio)
+library(phytools)
+library(geiger)
+library(randomcoloR)
+library(DBI) # SQLite-Library
+library(glue) # for making nice text
+# import custom functions
+source("4-1_functions.R")
+
+# set outgroup for rerooting. 298 is Petromyzon marinus, 359 is Asterias rubens, 362 is Pecten maximus
+outgroup <- "359"
+# set threshold modifier
+threshold <- "thr0_35"
+# set aster version modifier
+aster_ver <- "astralpro3"
+# set layout for export tree. Can be 'rectangular', 'roundrect', 'radial', 'circular', ' ellipse'.
+tree_layout <- "rectangular"
+
+
+# set import tree and modificator for saving pdfs
+treefile <- paste("data/treefiles-",threshold,"/all-loci_",aster_ver,".treefile", sep = '', collapse = NULL)
+savemod <- paste("r", outgroup, "-",threshold,"-",aster_ver,"-",tree_layout, sep = "", collapse = NULL)
+out_dir <- paste("output/",savemod,"/",sep="")
+
+# make output subdirectory if it doesn't exist
+if (!file.exists(out_dir))
+{ dir.create(out_dir) }
+
+# open the connection to the log file
+log_file <- file(paste(out_dir,savemod,"_README.log",sep=""),open="wt",blocking=FALSE)
+
+# import data matrix from SQLite database
+db_conn <- dbConnect(RSQLite::SQLite(), "data/genotree_master_library_corrected.db")
+matrix_query <- "SELECT ids.IDX, ids.AccessionNumber, taxonomy.ScientificName, taxonomy.Authority, taxonomy.Phylum, taxonomy.Gigaclass, taxonomy.Class, taxonomy.'Order', taxonomy.Family, taxonomy.taxGroup, traits.isMarine, traits.isBrackish, traits.isFresh, traits.isAllWater, traits.isMarineFresh, traits.isTerrestrial, traits.isMigratory
+FROM ids 
+INNER JOIN taxonomy 
+ON ids.IDX = taxonomy.IDX
+INNER JOIN traits
+ON ids.IDX = traits.IDX"
+data_matrix <- dbGetQuery(db_conn, matrix_query)
+outgroup_name <- dbGetQuery(db_conn, glue_sql(.con=db_conn,"SELECT ScientificName FROM taxonomy WHERE IDX={outgroup}"))$ScientificName
+dbDisconnect(db_conn)
+
+headline <- glue("Rooted on {outgroup_name} (IDX {outgroup}).\nThreshold value {threshold}, ASTER version of original consensus tree was {aster_ver}.")
+writeLines(headline,con=log_file,sep="\n")
+
+# set tip names for all output trees
+data_matrix$refTipName <- paste("(",data_matrix$IDX,")",sep="")
+data_matrix$mainTipName <- paste(data_matrix$ScientificName," (",data_matrix$IDX,")",sep="")
+
+# import the specified treefile and reroot it at the specified outgroup
+rerooted_tree <- import_tree(treefile, outgroup)
+
+# rename taxa in rerooted tree
+renamed_tree <- rename_taxa(rerooted_tree, data_matrix, key = 1, value = mainTipName)
+
+# make tree visualization basic plot and set basic parameters
+base_plot <- ggtree(renamed_tree,layout = tree_layout)
+base_plot <- base_plot+theme_tree2()+geom_rootpoint()+geom_tiplab(offset=0)+theme_tree()
+
+# make reference plot without renamed tips
+make_reference(rerooted_tree, paste(out_dir, savemod, "_ReferencePlot.pdf", sep = ""))
+
+
+### Colorblocked Trees ###
+# set output path
+path_colored <- paste(out_dir,savemod,"_ColoredPlot_",sep="")
+
+# set a vector with annotation colors and make tree annotated with class
+anno_colors_class <- c("green3","blue","brown","purple4","blue","violet","green2","grey","blue","black","blue","green4","pink4","red4")
+annotate_by_taxgroup("Class",base_plot,path_colored,col_vec = anno_colors_class)
+annotate_by_taxgroup("Class",base_plot,path_colored,col_vec = anno_colors_class, include = c("Teleostei","Holostei","Chondrostei","Cladistii","Elasmobranchii"))
+
+# set a vector with annotation colors and make tree annotated with taxGroup
+anno_colors_taxgroup <- c("green3","blue","brown","purple4","violet","green2","grey","black","green4","pink4","red4","grey","darkgrey")
+annotate_by_taxgroup("taxGroup", base_plot, path_colored, col_vec = anno_colors_taxgroup)
+annotate_by_taxgroup("taxGroup", base_plot, path_colored, col_vec = anno_colors_taxgroup, include=c("Fish"))
+
+# make assorted annotated trees
+annotate_by_taxgroup("Phylum",base_plot,path_colored,colorscheme = "blue")
+annotate_by_taxgroup("Order",base_plot,path_colored,colorscheme = "blue")
+annotate_by_taxgroup("Family", base_plot,path_colored,colorscheme = "blue")
+
+# put base plot into new plot object
+main_plot_taxgroups <- base_plot
+# save uncolored tree as pdf
+pdf(file=paste(out_dir,savemod,"_MainPlot.pdf", sep=""), width=35, height=75)
+main_plot_taxgroups
+dev.off()
+
+
+### Subtrees ###
+path_subtree <- paste(out_dir,savemod,"_SubtreePlot_",sep="")
+
+
+# check ASTER version and use different nodes for subtrees
+if (aster_ver=="astral4") {
+  # block for ASTRAL-IV
+  make_subtree(407,"Birds",path_subtree,10,10)
+  make_subtree(405,"Sauropsida",path_subtree,10,15)
+  make_subtree(457,"Mammals",path_subtree,10,10)
+  make_subtree(400,"Fish",path_subtree,20,50)
+} else if (aster_ver=="astral-pro3") {
+  # block for ASTRAL-Pro3
+  make_subtree(419,"Birds",path_subtree,10,10)
+  make_subtree(417,"Sauropsida",path_subtree,10,15)
+  make_subtree(507,"Amphibians",path_subtree,10,10)
+  make_subtree(469,"Mammals",path_subtree,10,10)
+  make_subtree(412,"Fish",path_subtree,20,50)
+  make_subtree(569,"Salmoniformes",path_subtree,10,10)
+}
+
+
+
+### Trait-Trees ###
+# make a ggtree phylo plot for annotating with the traits
+main_plot_traits <- annotate_by_taxgroup("Order",base_plot,fill="none",export=FALSE)
+
+# convert Index column in data_matrix to character (ChatGPT)
+data_matrix$IDX <- as.character(data_matrix$IDX)
+# convert tree to a tibble for easier manipulation (ChatGPT)
+tree_data <- as_tibble(rerooted_tree)
+# join the data matrix with the tree data based on the tip labels (ChatGPT)
+tree_data <- tree_data %>%
+  left_join(data_matrix, by = c("label" = "IDX"))
+
+# map traits onto its own tree and save the trees as pdfs
+trait_tree(paste(out_dir,savemod,"_TraitPlot_Marine.pdf",sep=''),main_plot_traits,"isMarine","blue")
+trait_tree(paste(out_dir,savemod,"_TraitPlot_Fresh.pdf",sep=''),main_plot_traits,"isFresh","lightblue")
+trait_tree(paste(out_dir,savemod,"_TraitPlot_Brackish.pdf",sep=''),main_plot_traits,"isBrackish","purple")
+trait_tree(paste(out_dir,savemod,"_TraitPlot_AllWater.pdf",sep=''),main_plot_traits,"isAllWater","blue")
+trait_tree(paste(out_dir,savemod,"_TraitPlot_Terrestrial.pdf",sep=''),main_plot_traits,"isTerrestrial","orange")
+trait_tree(paste(out_dir,savemod,"_TraitPlot_Migration.pdf",sep=''),main_plot_traits,"isMigratory","orange")
+
+# close the connection to the log file
+close(log_file)
