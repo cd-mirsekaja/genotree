26d28a4067abb572c273fee14a17208987fc3db9
cd-mirsekaja <114259912+cd-mirsekaja@users.noreply.github.com>
2025-03-07 18:57:22 +0100

    fixed minor issues

--- utilities/calculate_pairwise_time.sh
+++ utilities/calculate_pairwise_time.sh
@@ -19,10 +19,10 @@ for file in $file_list; do
     sequence_count=$(grep -c "^>" "$file")
     comparison_count=$(echo $(( $sequence_count * ($sequence_count - 1) / 2 )))
     comparison_time=$(echo "scale=2; $comparison_count * $time_per_comp / 60" | bc -l)
-    comparison_hours_minutes=$(echo $comparison_time | awk '{h=int($1/60); m=$1%60; printf "%d hours %d minutes\n", h, m + (m-int(m)>=0.5)}')
+    comparison_hours_minutes=$(echo $comparison_time | awk '{h=int($1/60); m=$1%60; printf "%dh %dm\n", h, m + (m-int(m)>=0.5)}')
     
     echo $file_name: $sequence_count Sequences, amounts to $comparison_count pairwise comparisons.
-    echo "Approximate time until completion: $comparison_hours_minutes."
+    echo "Approximate time until completion: $comparison_time minutes ($comparison_hours_minutes)."
     echo 
     
 done
